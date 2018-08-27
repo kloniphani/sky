df39b5562bc4b19a415b42cd8e6a41c228dda999
kloniphani <kloniphani@gmail.com>
2018-08-27 13:36:11 +0200

    First omnet project

--- src/node.cc
+++ src/node.cc
@@ -13,10 +13,11 @@ using namespace omnetpp;
 
 class node : public cSimpleModule
 {
-  protected:
-    // The following redefined virtual function holds the algorithm.
-    virtual void initialize() override;
-    virtual void handleMessage(cMessage *msg) override;
+    protected:
+        // The following redefined virtual function holds the algorithm.
+        virtual void forwardMessage(cMessage *msg);
+        virtual void initialize() override;
+        virtual void handleMessage(cMessage *msg) override;
 };
 
 // The module class needs to be registered with OMNeT++
@@ -26,28 +27,37 @@ void node::initialize()
 {
     // Initialize is called at the beginning of the simulation.
     // To bootstrap the tic-toc-tic-toc process, one of the modules needs
-    // to send the first message. Let this be `tic'.
 
-    // Am I Tic or Toc?
-    if (strcmp("ch", getName()) == 0) {
-        // The `ev' object works like `cout' in C++.
-        EV << "Sending initial message\n";
-
-        // create and send first message on gate "out". "tictocMsg" is an
-        // arbitrary string which will be the name of the message object.
-        cMessage *msg = new cMessage("simpleMsg");
-        send(msg, "gate$o", -1);
+    if (par("sendMsgOnInit").boolValue() == true && getIndex() == 00)
+    {
+        char msgname[20];
+        sprintf(msgname, "%s: %d", getName(), getIndex());
+        cMessage *msg = new cMessage(msgname);
+        scheduleAt(0.0, msg);
     }
 }
 
 void node::handleMessage(cMessage *msg)
 {
-    // msg->getName() is name of the msg object, here it will be "tictocMsg".
-    EV << "Received message `" << msg->getName() << "', sending it out again\n";
+    if(getIndex() == 3)
+    {
+        EV << "Message " << msg << " arrived.\n";
+		delete msg;
+    }
+    else
+    {
+        // We need to forward the message.
+        forwardMessage(msg);
+    }
+}
 
-    // The handleMessage() method is called whenever a message arrives
-    // at the module. Here, we just send it to the other module, through
-    // gate `out'. Because both `tic' and `toc' does the same, the message
-    // will bounce between the two.
-    send(msg, "gate$o", -1); // send out the message
+void node::forwardMessage(cMessage *msg)
+{
+    // In this example, we just pick a random gate to send it on.
+    // We draw a random number between 0 and the size of gate `out[]'.
+    int n = gateSize("gate");
+    int k = intuniform(0, n-1);
+
+    EV << "Forwarding message " << msg << " on port out[" << k << "]\n";
+    send(msg, "gate$o", k);
 }
