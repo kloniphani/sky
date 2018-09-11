54ef898e8f5b90400bf0ed698e099101a719095e
kloniphani <kloniphani@gmail.com>
2018-09-11 19:24:20 +0200

    Reverted backup

---
+++ out/clang-release/src/node.d
@@ -0,0 +1,4 @@
+../out/clang-release/src/node.o: node.cc \
+  C:/omnetpp-5.4.1/include\omnetpp.h
+
+C:/omnetpp-5.4.1/include\omnetpp.h:
--- src/node.cc
+++ src/node.cc
@@ -1,5 +1,5 @@
 /*
- * Node.cc
+ * functionality.cc
  *
  *  Created on: Aug 25, 2018
  *      Author: Kloniphani
@@ -9,156 +9,56 @@
 #include <string.h>
 #include <omnetpp.h>
 
+
 using namespace omnetpp;
 
-#include ".\simpleMsg_m.h"
-
-class Node : public cSimpleModule
+class node : public cSimpleModule
 {
-    private:
-        long numSent;
-        long numReceived;
-        cLongHistogram hopCountStats;
-        cOutVector hopCountVector;
-
-        simsignal_t arrivalSignal;
-
     protected:
         // The following redefined virtual function holds the algorithm.
-        virtual simpleMsg *generateMessage();
-        virtual void forwardMessage(simpleMsg *msg);
-        virtual void refreshDisplay() const override;
-        virtual void finish() override;
-
+        virtual void forwardMessage(cMessage *msg);
         virtual void initialize() override;
         virtual void handleMessage(cMessage *msg) override;
 };
 
 // The module class needs to be registered with OMNeT++
-Define_Module(Node);
+Define_Module(node);
 
-void Node::initialize()
+void node::initialize()
 {
-    // Initialize variables
-    numSent = 0;
-    numReceived = 0;
-    WATCH(numSent);
-    WATCH(numReceived);
-
-    hopCountStats.setName("hopCountStats");
-    hopCountStats.setRangeAutoUpper(0, 10, 1.5);
-    hopCountVector.setName("HopCount");
-
-    arrivalSignal = registerSignal("arrival");
-
     // Initialize is called at the beginning of the simulation.
     // To bootstrap the tic-toc-tic-toc process, one of the modules needs
+
     if (par("sendMsgOnInit").boolValue() == true && getIndex() == 00)
     {
-        simpleMsg *msg = generateMessage();
-        numSent++;
+        char msgname[20];
+        sprintf(msgname, "%s: %d", getName(), getIndex());
+        cMessage *msg = new cMessage(msgname);
         scheduleAt(0.0, msg);
     }
 }
 
-
-void Node::handleMessage(cMessage *msg)
+void node::handleMessage(cMessage *msg)
 {
-    simpleMsg *ttmsg = check_and_cast<simpleMsg *>(msg);
-    if(ttmsg->getDestination() == getIndex())
+    if(getIndex() == 3)
     {
-        // Message arrived.
-        int hopcount = ttmsg->getHopCount();
-        EV << "Message " << ttmsg << " arrived after " << ttmsg->getHopCount() << " hops.\n";
-
-        // update statistics.
-        numReceived++;
-        hopCountVector.record(hopcount);
-        hopCountStats.collect(hopcount);
-
-        // send a signal
-        emit(arrivalSignal, hopcount);
-
-        if (hasGUI())
-        {
-            char label[50];
-            // Write last hop count to string
-            sprintf(label, "last hopCount = %d", hopcount);
-            // Get pointer to figure
-            cCanvas *canvas = getParentModule()->getCanvas();
-            cTextFigure *textFigure = check_and_cast<cTextFigure*>(canvas->getFigure("lasthopcount"));
-            // Update figure text
-            textFigure->setText(label);
-        }
-
-        bubble("ARRIVED, starting new one!");
-        delete msg;
-
-        // Generate another one.
-        EV << "Generating another message: ";
-        simpleMsg *newmsg = generateMessage();
-        EV << newmsg << endl;
-        forwardMessage(newmsg);
-        numSent++;
+        EV << "Message " << msg << " arrived.\n";
+		delete msg;
     }
     else
     {
         // We need to forward the message.
-        forwardMessage(ttmsg);
+        forwardMessage(msg);
     }
 }
 
-simpleMsg *Node::generateMessage()
+void node::forwardMessage(cMessage *msg)
 {
-    // Produce source and destination addresses.
-    int src = getIndex();  // our module index
-    int n = getVectorSize();  // module vector size
-    int dest = intuniform(0, n-2);
-    if (dest >= src)
-        dest++;
-
-    char msgname[20];
-    sprintf(msgname, "tic-%d-to-%d", src, dest);
-
-    // Create message object and set source and destination field.
-    simpleMsg *msg = new simpleMsg(msgname);
-    msg->setSource(src);
-    msg->setDestination(dest);
-    return msg;
-}
-
-void Node::forwardMessage(simpleMsg *msg)
-{
-    // Increment hop count.
-    msg->setHopCount(msg->getHopCount()+1);
-
-    // Same routing as before: random gate.
+    // In this example, we just pick a random gate to send it on.
+    // We draw a random number between 0 and the size of gate `out[]'.
     int n = gateSize("gate");
     int k = intuniform(0, n-1);
 
-    EV << "Forwarding message " << msg << " on gate[" << k << "]\n";
+    EV << "Forwarding message " << msg << " on port out[" << k << "]\n";
     send(msg, "gate$o", k);
-}
-
-void Node::refreshDisplay() const
-{
-    char buf[40];
-    sprintf(buf, "rcvd: %ld sent: %ld", numReceived, numSent);
-    getDisplayString().setTagArg("t", 0, buf);
-}
-
-void Node::finish()
-{
-    // This function is called by OMNeT++ at the end of the simulation.
-    EV << "Sent:     " << numSent << endl;
-    EV << "Received: " << numReceived << endl;
-    EV << "Hop count, min:    " << hopCountStats.getMin() << endl;
-    EV << "Hop count, max:    " << hopCountStats.getMax() << endl;
-    EV << "Hop count, mean:   " << hopCountStats.getMean() << endl;
-    EV << "Hop count, stddev: " << hopCountStats.getStddev() << endl;
-
-    recordScalar("#sent", numSent);
-    recordScalar("#received", numReceived);
-
-    hopCountStats.recordAs("hop count");
 }
